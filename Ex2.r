89cc0f144308412274f501a0ace59b39a58afd79
IZerab <beltramelorenzo1999@gmail.com>
2021-12-07 09:17:34 +0100

    Weekly exercise done
    I've finished week 4 exercises.
    The code requires:
    
    - glmnet
    -  selectiveInference

--- Ex2.r
+++ Ex2.r
@@ -5,56 +5,102 @@
 library(glmnet)
 library(selectiveInference)
 
+# set the seed
+set.seed(200)
+
 # set working directory
-setwd("/Users/Lorenzo/GitHub/Stats")
+setwd("C:/Users/beltr/Documents/GitHub/Stats")
+
+
 
 # preparing the datset
-data <- read.csv("communities.data", header = FALSE)
+data <- read.csv(file = "communities.csv", header = FALSE)
 n <- dim(data)[1]
-p <- dim(data)[2]
-Y <- data[,d]
-
-# in my design matrix I do not wat community identifiers!
+d <- dim(data)[2]
+y <- data[, d]
+# remove non-predictor columns (metadata, i.e. community identifier)
 X <- data[, -c(1:5, d)]
-
-p <- dime(X)[2]
+p <- dim(X)[2]
 X <- matrix(as.numeric(as.matrix(X)), nrow = n)
 bool <- rowSums(is.na(X)) == 0
-X <- X[bool,]
-Y <- Y[bool,]
+X <- X[bool, ]
+y <- y[bool]
 
-
-# get the names of the columns (have been saven in a separated file!)
-colnames <- read.table(file="names.csv", header=FALSE, sep="\n")
-colnames <- as.character(unlist(colnames))[-(1:11)]
-
-# clean what I imported via substitution
+# adding the names of the columns
+colnames <- read.csv(file = "names.csv", header = FALSE, sep = "\n")
+# get rid of useless metadata
+colnames <- as.character(unlist(colnames))[-(1:5)]
 colnames <- sub("@attribute ", "", colnames)
 colnames <- sub(" numeric\\\\", "", colnames)
+colnames <- sub(" numeric", "", colnames)
 colnames <- sub(" numeric}", "", colnames)[1:p]
-
 colnames(X) <- colnames
 
 
 # I decide to work with less samples
 n <- 100
-set.seed(42)
-
 subsample <- sample(1:nrow(X), n)
-X <- X[subsample]
-Y <- Y[subsample]
+X <- X[subsample, ]
+y <- y[subsample]
 p <- dim(X)[2]
 
 
+# LASSO model selection
+
+lambda <- 0.058
+# choose to have only the best 7 features
+M <- glmnet(X, y, lambda = lambda)
+coeffs <- coef(M)
+# print only the non null coeff
+chosen_coef <- colnames[coeffs@i]
+print("The selected features are: ")
+print(chosen_coef) # selected variables
+
+# naive inference
+# fit a linear model (OLS) using only the selected variables
+fitM <- lm(y ~ X[, chosen_coef])
+print(summary(fitM))
+# post-model-selection estimate of the error standard deviation (overestimates the true unknown error variance)
+sigma <- summary(fitM)$sigma
+print(paste0("Sigma is: ", sigma))
+
+# conditional PoSI
+lassoInference <- fixedLassoInf(
+    x = X,
+    y = y,
+    family = "gaussian",
+    beta = coef(M, s = lambda / n)[-1],
+    lambda = lambda,
+    alpha = 0.05
+)
+print(lassoInference)
 
 
+# uniform PoSI
+B.PoSI <- function(alpha, q, N, Nsim = 10000, t.eps = 0.001) {
+    G2 <- rchisq(Nsim, df = q)
+    t <- seq(0, max(sqrt(G2)), by = t.eps)
+    E_G <- 1
+    i <- 1
+    while (E_G[i] > alpha) {
+        mins <- pmin(1, N * (1 - pbeta(t[i]^2 / G2, shape1 = 1 / 2, shape2 = (q - 1) / 2)))
+        E_G <- c(E_G, mean(mins))
+        i <- i + 1
+    }
+    # plot(t[1:i], E_G, type="l")
+    # abline(h=alpha)
+    return(t[i])
+}
 
-graphics.off()  # clear all graphs
-    rm(list = ls()) # remove all files from your workspace
-    
-    n <- 500 # number of observations
-    p <- 20  # number of variables
+
+# if we want to protect against ANY model selection procedure, we should use
+kappa <- sum((1:p + 1) * choose(p, 1:p))
+result <- B.PoSI(0.05, min(kappa, p, n), kappa, Nsim = 10000, t.eps = 0.01)
+
+print(paste0("The resulting B.PoSI: ", result))
 
 
-# FIRST TASK: naive approach
-
+# adjust the naive inference
+summary(fitM)
+# Compare absolute t-statistics to critical value B.PoSI. Only var45 is still significant. When protecting against ANY model selection procedure, not even var45 is significant any more.
+colnames[c(45)] 
